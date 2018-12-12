45998f44a1db3b348986a941e39c2d72022ddd13
edervs <eedervs.97@gmail.com>
2018-12-12 17:04:33 -0600

    Agregando comentarios Se agregaron los comentarios a attributes e intermediate_code

--- Lexer/attributes.c
+++ Lexer/attributes.c
@@ -1,8 +1,31 @@
+/* 
+ * Archivo: attributes.c
+ * Authors: Concha Vázquez Miguel
+ * 			Flores Martínez Andrés
+ * 			Gladín García Ángel Iván
+ *  		Sánchez Pérez Pedro Juan Salvador
+ *			Vázquez Salcedor Eduardo Eder
+ * 			
+ * Creado el 5 de diciembre de 2018.
+ */
+
 #include "attributes.h"
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+ 
 
+/**
+ * Quita la etiqueta que está en el tope de la pila de etiquetas enviada.
+ * 
+ * Autores: Concha Vázquez Miguel
+ * 			Flores Martínez Andrés
+ * 			Gladín García Ángel Iván
+ *  		Sánchez Pérez Pedro Juan Salvador
+ *			Vázquez Salcedor Eduardo Eder
+ * 
+ * Creada el 5 de diciembre de 2018.
+ */
 char *pop_label(labels* ls){
     char *t= (char*) malloc(32*sizeof(char));
     if(ls->count>-1){        
@@ -12,6 +35,17 @@ char *pop_label(labels* ls){
     return t;
 }
 
+/**
+ * Agrega una etiqueta en el tope de la pila de etiquetas enviada.
+ * 
+ * Autores: Concha Vázquez Miguel
+ * 			Flores Martínez Andrés
+ * 			Gladín García Ángel Iván
+ *  		Sánchez Pérez Pedro Juan Salvador
+ *			Vázquez Salcedor Eduardo Eder
+ * 
+ * Creada el 5 de diciembre de 2018.
+ */
 void push_label(labels* ls, char *l){
     if(ls->count<100){
         strcpy(ls->label[ls->count], l);
@@ -19,18 +53,62 @@ void push_label(labels* ls, char *l){
     }
 }
 
+/**
+ * Regresa la etiqueta que está en el tope de la pila de etiquetas enviada.
+ * 
+ * Autores: Concha Vázquez Miguel
+ * 			Flores Martínez Andrés
+ * 			Gladín García Ángel Iván
+ *  		Sánchez Pérez Pedro Juan Salvador
+ *			Vázquez Salcedor Eduardo Eder
+ * 
+ * Creada el 5 de diciembre de 2018.
+ */
 char *get_top_label(labels* ls){
     return ls->label[ls->count-1];
 }
 
+/**
+ * Regresa la etiqueta que está debajo del tope de la pila de etiquetas enviada.
+ * 
+ * Autores: Concha Vázquez Miguel
+ * 			Flores Martínez Andrés
+ * 			Gladín García Ángel Iván
+ *  		Sánchez Pérez Pedro Juan Salvador
+ *			Vázquez Salcedor Eduardo Eder
+ * 
+ * Creada el 5 de diciembre de 2018.
+ */
 char *get_top_label_previous(labels* ls){
     return ls->label[ls->count-2];
 }
 
+/**
+ * Asigna el count de la pila de etiquetas a cero.
+ * 
+ * Autores: Concha Vázquez Miguel
+ * 			Flores Martínez Andrés
+ * 			Gladín García Ángel Iván
+ *  		Sánchez Pérez Pedro Juan Salvador
+ *			Vázquez Salcedor Eduardo Eder
+ * 
+ * Creada el 5 de diciembre de 2018.
+ */
 void create_labels(labels* ls){
     ls->count=0;
 }
 
+/**
+ * Imprime las etiquetas en forma de pila.
+ * 
+ * Autores: Concha Vázquez Miguel
+ * 			Flores Martínez Andrés
+ * 			Gladín García Ángel Iván
+ *  		Sánchez Pérez Pedro Juan Salvador
+ *			Vázquez Salcedor Eduardo Eder
+ * 
+ * Creada el 5 de diciembre de 2018.
+ */
 void print_stack_labels(labels* ls){
     int i;
     printf("LABELS\n");
@@ -39,6 +117,17 @@ void print_stack_labels(labels* ls){
     }
 }
 
+/**
+ * Imprime las etiquetas en forma de lista.
+ * 
+ * Autores: Concha Vázquez Miguel
+ * 			Flores Martínez Andrés
+ * 			Gladín García Ángel Iván
+ *  		Sánchez Pérez Pedro Juan Salvador
+ *			Vázquez Salcedor Eduardo Eder
+ * 
+ * Creada el 5 de diciembre de 2018.
+ */
 void print_list_labels(labels* ls){
     int i;
     printf("LABELS\n");
