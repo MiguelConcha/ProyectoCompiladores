749db6c193fcd62e01ca616d48d1c3888d472f0a
Miguel Concha <wormb0t@Miguels-MacBook-Pro.local>
2018-12-12 17:17:42 -0600

    Comentando backpatch.h

--- Lexer/backpatch.h
+++ Lexer/backpatch.h
@@ -1,15 +1,26 @@
+/* 
+ * Archivo: backpatch.h
+ * Autores: Concha Vázquez Miguel
+ *          Flores Martínez Andrés
+ *          Gladín García Ángel Iván
+ *          Sánchez Pérez Pedro Juan Salvador
+ *          Vázquez Salcedor Eduardo Eder
+ *          
+ * Creado el 10 de diciembre de 2018.
+ */
+
 #ifndef BACKPATCH_H
 #define BACKPATCH_H
 
 #include "attributes.h"
 #include "intermediate_code.h"
     
+/* Prototipos para las funciones necesarias para el backpatch.
+   Para ahondar en su funcionamiento, consultar el archivo backpatch.c. */
 labels create_list(char*);
-
 labels create_empty_list();
 labels merge(labels*, labels*);
 int backpatch(labels*, char*, ic*);
 char *get_first(labels*);
-
 
 #endif
--- Lexer/maik.sh
+++
@@ -1,5 +0,0 @@
-clear;
-flex lexer.l;
-bison -dv parser.y;
-g++ lex.yy.c parser.tab.c -o prog;
-./prog testLUNES.txt
